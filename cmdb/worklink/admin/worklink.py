141580673630d51b7ab42871c555fdac2579644d
liuzheng <liuzheng712@gmail.com>
2020-06-06 11:30:01 +0800

    update

--- cmdb/worklink/admin/worklink.py
+++ cmdb/worklink/admin/worklink.py
@@ -5,4 +5,5 @@ from cmdb.mixin import MixinAdmin
 
 @admin.register(models.WorkLink)
 class WorkLinkAdmin(MixinAdmin):
-    pass
+    list_display = ('Name', 'Link', 'CreateDate')
+    search_fields = ('Name', 'Link')
